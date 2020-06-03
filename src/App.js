b22af39e4595aaefdfb98ac36532f3864bb597e4
Muhammad Nouman Atiq <36809880+nomisatti@users.noreply.github.com>
2020-06-03 13:23:55 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Change made by <b>Muhammad Nouman Atiq</b></p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
